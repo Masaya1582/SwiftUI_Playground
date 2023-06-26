ab572a99ee90c19dd3482f853cf54c01e4d36b9b
Masaya1582 <masa8y88@gmail.com>
2023-06-26 16:28:31 +0900

    Initial Commit

---
+++ SwiftUI_Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI_Playground
+//
+//  Created by MasayaNakakuki on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
