def170d5d2c6a83e043276db6c89c8eb32ce0ea2
Masaya Nakakuki <masa8y88@gmail.com>
2023-06-30 07:07:24 +0900

    feat: FocusStateの使い方その2

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -7,42 +7,67 @@
 
 import SwiftUI
 
+enum FocusField: String, RawRepresentable {
+    case first
+    case second
+}
+
 struct HomeView: View {
-    @State private var textFieldInput: String = ""
-    @FocusState private var foucusValue: Bool
+    @State private var firstValue: String = ""
+    @State private var secondValue: String = ""
+    @FocusState private var focusField: FocusField?
 
     var body: some View {
-        VStack(spacing: 32) {
-            Text("Focus: \(foucusValue == true ? "True" : "False" )")
-                .font(.system(size: 24, weight: .bold))
-
-            TextField("Your Texts Here", text: $textFieldInput)
-                .focused($foucusValue)
-                .textFieldStyle(.roundedBorder)
-
-            Button(action: {
-                foucusValue = true
-            }, label: {
-                Text("Focus on TextField")
+        ZStack {
+            Color.gray.opacity(0.4)
+                .ignoresSafeArea()
+            VStack(spacing: 32) {
+                Text("Focus Status: \(focusField?.rawValue ?? "Unfocused" )")
                     .font(.system(size: 24, weight: .bold))
-                    .frame(width: 300, height: 60, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(.orange)
-                    .cornerRadius(12)
-            })
 
-            Button(action: {
-                foucusValue = false
-            }, label: {
-                Text("Unfocus TextField")
-                    .font(.system(size: 24, weight: .bold))
-                    .frame(width: 300, height: 60, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(.green)
-                    .cornerRadius(12)
-            })
+                TextField("First TextField", text: $firstValue)
+                    .focused($focusField, equals: .first)
+                    .textFieldStyle(.roundedBorder)
+
+                TextField("Second TextField", text: $secondValue)
+                    .focused($focusField, equals: .second)
+                    .textFieldStyle(.roundedBorder)
+
+                Button(action: {
+                    focusField = .first
+                }, label: {
+                    Text("Focus on First")
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(width: 300, height: 60, alignment: .center)
+                        .foregroundColor(.white)
+                        .background(.green)
+                        .cornerRadius(12)
+                })
+
+                Button(action: {
+                    focusField = .second
+                }, label: {
+                    Text("Focus on Second")
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(width: 300, height: 60, alignment: .center)
+                        .foregroundColor(.white)
+                        .background(.yellow)
+                        .cornerRadius(12)
+                })
+
+                Button(action: {
+                    focusField = nil
+                }, label: {
+                    Text("UnFocus TextField")
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(width: 300, height: 60, alignment: .center)
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(12)
+                })
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
