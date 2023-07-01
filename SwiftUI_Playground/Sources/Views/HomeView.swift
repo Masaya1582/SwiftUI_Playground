027d7fd15a6d723666cbde6d43ded5841187d3f4
Masaya1582 <masa8y88@gmail.com>
2023-07-01 14:33:00 +0900

    feat: 生体認証の実装

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct HomeView: View {
+    @State private var isUnlocked = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            if isUnlocked {
+                Text("App Unlocked")
+                    .font(.largeTitle)
+                Image(systemName: "lock.open")
+                    .resizable()
+                    .frame(width: 100, height: 140)
+            } else {
+                Text("App Locked")
+                    .font(.largeTitle)
+                Image(systemName: "lock.fill")
+                    .resizable()
+                    .frame(width: 100, height: 140)
+            }
         }
-        .padding()
+        .onAppear(perform: authenticate)
+    }
+
+    private func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    print("DEBUG_PRINT: Authentication Failed!")
+                }
+            }
+        } else {
+            print("DEBUG_PRINT: No Biometrics!")
+        }
     }
 }
 
