fa5fdce0aa1da10b0feb258d15cfa6216352f5aa
Masaya1582 <masa8y88@gmail.com>
2023-07-03 11:34:46 +0900

    feat: AsyncとAwaitの練習

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var pokemons: [Pokemon] = []
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ForEach(pokemons) { pokemon in
+                HStack {
+                    Image(uiImage: pokemon.image)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    VStack(alignment: .leading) {
+                        Text("No: \(pokemon.id)")
+                        Text(pokemon.name)
+                        Text(pokemon.name)
+                    }
+                    Spacer()
+                }
+                Divider()
+            }
         }
-        .padding()
+        .task {
+            guard let pokemons = try? await fetchPokemons() else {
+                return
+            }
+            await MainActor.run {
+                self.pokemons = pokemons
+            }
+        }
+    }
+
+    private func fetchPokemons() async throws -> [Pokemon] {
+        let host = "https://swiswiswift.com/2022-06-16"
+        guard let url = URL(string: "\(host)/pokemons.json") else {
+            fatalError("URL is not valid")
+        }
+        print(url)
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let pokemonResponses = try JSONDecoder().decode([PokemonResponse].self, from: data)
+
+        var pokemons: [Pokemon] = []
+        for pokemonResponse in pokemonResponses {
+            guard let imageUrl = URL(string: "\(host)/\(pokemonResponse.imageName)") else {
+                fatalError("URL not found")
+            }
+            let (data, _) = try await URLSession.shared.data(from: imageUrl)
+            guard let image = UIImage(data: data) else {
+                fatalError("Image not found")
+            }
+            let pokemon = Pokemon(
+                id: pokemonResponse.id,
+                name: pokemonResponse.name,
+                imageName: pokemonResponse.name,
+                image: image)
+            pokemons.append(pokemon)
+        }
+        return pokemons
     }
 }
 
