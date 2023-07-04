68ff4f7353bd5e9198fc04a1e914441bbe5db981
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-04 14:24:42 +0900

    Textの折りたたみ

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ScrollView {
+            VStack(spacing: 24) {
+                ShowMoreView(text: L10n.trumpOne)
+                ShowMoreView(text: L10n.trumpTwo)
+                ShowMoreView(text: L10n.trumpThree)
+            }
+        }        
     }
 }
 
