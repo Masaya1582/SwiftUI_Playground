ec50103de20b0cf626485477b8aee4fa6fb31487
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-04 14:34:44 +0900

    Map上にピンを立てる

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct HomeView: View {
+    @State private var showingAlert = false
+    @State private var selectedLocation: CLLocationCoordinate2D?
+    @State private var locations: [CLLocationCoordinate2D] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        MapView(locations: $locations) { location in
+            self.selectedLocation = location
+            self.showingAlert = true
         }
-        .padding()
+        .ignoresSafeArea()
+        .alert(isPresented: $showingAlert) {
+            Alert(
+                title: Text(""),
+                message: Text("Latitude: \(selectedLocation?.latitude ?? -1), Longitude: \(selectedLocation?.longitude ?? -1)"),
+                primaryButton: .default(Text("OK")) {
+                    guard let location = selectedLocation else {
+                        return
+                    }
+                    self.addLocation(location)
+                }, secondaryButton: .cancel())
+        }
+    }
+
+    private func addLocation(_ location: CLLocationCoordinate2D) {
+        locations.append(location)
     }
 }
 
