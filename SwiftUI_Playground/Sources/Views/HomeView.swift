22efb551d99df8cb40f051968583fe40114a9f05
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-07 11:24:57 +0900

    タッチ感覚(色々ある)

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -9,11 +9,42 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Button {
+                let generator = UIImpactFeedbackGenerator(style: .light)
+                generator.impactOccurred()
+            } label: {
+                Text("Light Touch")
+                    .font(.custom(FontFamily.Caprasimo.regular, size: 42))
+            }
+            Button {
+                let generator = UIImpactFeedbackGenerator(style: .medium)
+                generator.impactOccurred()
+            } label: {
+                Text("Midium Touch")
+                    .font(.custom(FontFamily.Caprasimo.regular, size: 42))
+            }
+            Button {
+                let generator = UIImpactFeedbackGenerator(style: .heavy)
+                generator.impactOccurred()
+            } label: {
+                Text("Heavy Touch")
+                    .font(.custom(FontFamily.Caprasimo.regular, size: 42))
+            }
+            Button {
+                let generator = UIImpactFeedbackGenerator(style: .soft)
+                generator.impactOccurred()
+            } label: {
+                Text("Soft Touch")
+                    .font(.custom(FontFamily.Caprasimo.regular, size: 42))
+            }
+            Button {
+                let generator = UIImpactFeedbackGenerator(style: .rigid)
+                generator.impactOccurred()
+            } label: {
+                Text("Rigid Touch")
+                    .font(.custom(FontFamily.Caprasimo.regular, size: 42))
+            }
         }
         .padding()
     }
