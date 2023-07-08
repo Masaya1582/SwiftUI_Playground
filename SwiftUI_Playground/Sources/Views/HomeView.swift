5cec4704206b1f8c47d13737532d69008aa168f4
Masaya1582 <masa8y88@gmail.com>
2023-07-08 10:35:56 +0900

    Deviceのシェイクを検知

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var title: String = "Shake"
+    @State private var number: Int = 0
+
     var body: some View {
-        VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
-        }
+        Text("\(title): \(number)")
+            .font(.custom(FontFamily.Caprasimo.regular, size: 32))
+            .onReceive(NotificationCenter.default.publisher(for: .deviceDidShakeNotification)) { _ in
+                number += 1
+            }
+    }
+}
+
+extension NSNotification.Name {
+    public static let deviceDidShakeNotification = NSNotification.Name("DeviceDidShakeNotification")
+}
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        NotificationCenter.default.post(name: .deviceDidShakeNotification, object: event)
     }
 }
 
