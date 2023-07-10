d4ccc5aa1c607d519dd53bb1d7b57d811cb33a70
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-10 11:39:04 +0900

    Combine、Flatmapの使い方

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var viewModel = FlatMapViewModel()
+
     var body: some View {
         VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
+            Text("Perform FlatMap Operation")
+                .font(.title)
+                .padding()
+            Button("FlatMap") {
+                viewModel.performFlatMap()
+            }
+            .font(.custom(FontFamily.Caprasimo.regular, size: 32))
+            .padding()
         }
     }
 }
