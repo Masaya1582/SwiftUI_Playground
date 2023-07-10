0c95c878009c8d81f470516b88fff8431a52f90e
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-10 15:10:43 +0900

    Enumで色を出し分け

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+enum ColorChoice: String, CaseIterable {
+    case red
+    case green
+    case blue
+    case yellow
+
+    var color: Color {
+        switch self {
+        case .red:
+            return .red
+        case .green:
+            return .green
+        case .blue:
+            return .blue
+        case .yellow:
+            return .yellow
+        }
+    }
+
+    var displayName: String {
+        return self.rawValue.capitalized
+    }
+}
+
 struct HomeView: View {
+    @State private var selectedColor: ColorChoice = .red
+
     var body: some View {
         VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
+            Text("Selected Color:")
+                .font(.custom(FontFamily.Caprasimo.regular, size: 32))
+                .padding()
+
+            Circle()
+                .foregroundColor(selectedColor.color)
+                .frame(width: 200, height: 200)
+
+            Picker(selection: $selectedColor, label: Text("Select a Color")) {
+                ForEach(ColorChoice.allCases, id: \.self) { color in
+                    Text(color.displayName)
+                        .tag(color)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
         }
     }
 }
