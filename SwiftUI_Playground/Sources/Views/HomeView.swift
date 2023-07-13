22181d24c3183cef017f78276b17b92ed7b7111e
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-13 15:44:53 +0900

    UIBezierPathとCGPathを使う

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -7,21 +7,44 @@
 
 import SwiftUI
 
+struct ScaledBezier: Shape {
+    let bezierPath: UIBezierPath
+
+    func path(in rect: CGRect) -> Path {
+        let path = Path(bezierPath.cgPath)
+        // Figure out how much bigger we need to make our path in order for it to fill the available space without clipping.
+        let multiplier = min(rect.width, rect.height)
+        // Create an affine transform that uses the multiplier for both dimensions equally.
+        let transform = CGAffineTransform(scaleX: multiplier, y: multiplier)
+        // Apply that scale and send back the result.
+        return path.applying(transform)
+    }
+}
+
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
-        }
+        ScaledBezier(bezierPath: .logo)
+            .stroke(lineWidth: 2)
+            .frame(width: 200, height: 200)
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+extension UIBezierPath {
+    /// The Unwrap logo as a Bezier path.
+    static var logo: UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0.534, y: 0.5816))
+        path.addCurve(to: CGPoint(x: 0.1877, y: 0.088), controlPoint1: CGPoint(x: 0.534, y: 0.5816), controlPoint2: CGPoint(x: 0.2529, y: 0.4205))
+        path.addCurve(to: CGPoint(x: 0.9728, y: 0.8259), controlPoint1: CGPoint(x: 0.4922, y: 0.4949), controlPoint2: CGPoint(x: 1.0968, y: 0.4148))
+        path.addCurve(to: CGPoint(x: 0.0397, y: 0.5431), controlPoint1: CGPoint(x: 0.7118, y: 0.5248), controlPoint2: CGPoint(x: 0.3329, y: 0.7442))
+        path.addCurve(to: CGPoint(x: 0.6211, y: 0.0279), controlPoint1: CGPoint(x: 0.508, y: 1.1956), controlPoint2: CGPoint(x: 1.3042, y: 0.5345))
+        path.addCurve(to: CGPoint(x: 0.6904, y: 0.3615), controlPoint1: CGPoint(x: 0.7282, y: 0.2481), controlPoint2: CGPoint(x: 0.6904, y: 0.3615))
+        return path
     }
 }
