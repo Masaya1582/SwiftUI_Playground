018fde1f3526736a6dc2e131983e4d73c8a15ae7
Masaya1582 <masa8y88@gmail.com>
2023-07-16 12:30:16 +0900

    iPhoneのフラッシュライトのオンオフを制御する

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct HomeView: View {
+    @State private var isFlashlightOn = false
+
     var body: some View {
-        VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
+        VStack(spacing: 32) {
+            Image(systemName: isFlashlightOn ? "flashlight.on.fill" : "flashlight.off.fill")
                 .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+            Button(action: {
+                toggleFlashlight()
+            }) {
+                Text(isFlashlightOn ? "Turn Off" : "Turn On")
+                    .font(.custom(FontFamily.Caprasimo.regular, size: 24))
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width / 1.3, height: UIScreen.main.bounds.height / 28, alignment: .center)
+                    .padding()
+                    .background(isFlashlightOn ? Color.red : Color.green)
+                    .cornerRadius(10)
+            }
+        }
+    }
+
+    private func toggleFlashlight() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        if let device = device, device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                device.torchMode = isFlashlightOn ? .off : .on
+                device.unlockForConfiguration()
+                isFlashlightOn.toggle()
+            } catch {
+                print("Torch could not be used.")
+            }
         }
     }
 }
