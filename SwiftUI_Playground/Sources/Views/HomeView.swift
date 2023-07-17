1a7f16030e6d4e97fa8853e43cb12ce892e8584c
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-17 11:07:59 +0900

    Stepperを使った入力

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var age = 18
+
     var body: some View {
-        VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
+        VStack(spacing: 20) {
+            Stepper("Enter your age", value: $age, in: 0...130)
+            Text("Your age is \(age)")
+                .font(.custom(FontFamily.Caprasimo.regular, size: 24))
+
+            Stepper("Enter your age(違った書き方)", onIncrement: {
+                age += 1
+            }, onDecrement: {
+                age -= 1
+            })
+            Text("Your age is \(age)")
+                .font(.custom(FontFamily.Caprasimo.regular, size: 24))
         }
+        .padding()
     }
 }
 
