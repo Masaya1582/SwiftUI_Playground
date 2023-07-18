cb2c5c82d26b2d2a9cdf5cc691569f022a7207c8
Masaya1582 <masa8y88@gmail.com>
2023-07-18 18:28:39 +0900

    投稿用に修正

--- SwiftUI_Playground/Sources/Views/CustomHalfView.swift
+++ SwiftUI_Playground/Sources/Views/CustomHalfView.swift
@@ -9,47 +9,35 @@
 import SwiftUI
 
 struct CustomHalfView: View {
-    @State private var selectedTheme = "Dark"
-    private let themes = ["Dark", "Light", "Automatic"]
+    @State private var selectedTheme = "book"
+    private let themes = ["book", "eyeglasses", "timer"]
 
     var body: some View {
         NavigationStack {
             Form {
-                Section {
-                    // デフォルト
-                    Picker("Appearance", selection: $selectedTheme) {
-                        ForEach(themes, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                }
-
-                // インラインスタイル
                 Picker("Appearance", selection: $selectedTheme) {
-                    ForEach(themes, id: \.self) {
-                        Text($0)
-                    }
-                }
-                .pickerStyle(.inline)
-
-                // Wheel style
-                Picker("Appearance", selection: $selectedTheme) {
-                    ForEach(themes, id: \.self) {
-                        Text($0)
-                    }
-                }
-                .pickerStyle(.wheel)
-
-                // Segmentedスタイル
-                Picker("Appearance", selection: $selectedTheme) {
-                    ForEach(themes, id: \.self) {
-                        Text($0)
+                    ForEach(themes, id: \.self) { themes in
+                        Image(systemName: themes)
                     }
                 }
                 .pickerStyle(.segmented)
                 .pickerStyle(.navigationLink)
+                switch selectedTheme {
+                case "book":
+                    Text("Book View")
+                        .font(.custom(FontFamily.Caprasimo.regular, size: 32))
+                case "eyeglasses":
+                    Text("Eyeglasses View")
+                        .font(.custom(FontFamily.Caprasimo.regular, size: 32))
+                case "timer":
+                    Text("Timer View")
+                        .font(.custom(FontFamily.Caprasimo.regular, size: 32))
+                default:
+                    EmptyView()
+                }
             }
-            .navigationTitle("Display & Brightness")
+            .navigationTitle("This is Half-ModalView")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
