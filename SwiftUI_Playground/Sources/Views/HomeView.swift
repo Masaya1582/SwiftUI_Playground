7fc82537e81d0956e347d21323e89eda2e2be50d
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-20 10:01:56 +0900

    SQLiteの使い方

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,16 +8,50 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var itemName = ""
+    @State private var items: [String] = []
+
     var body: some View {
         VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .frame(width: 320, height: 280)
-            Spacer().frame(height: 100)
+            TextField("Enter item name", text: $itemName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button("Add Item") {
+                addItem()
+            }
+            .padding()
+
+            List(items, id: \.self) { item in
+                HStack {
+                    Text(item)
+                    Spacer()
+                    Button("Delete") {
+                        deleteItem(name: item)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            fetchItems()
         }
     }
+
+    private func fetchItems() {
+        items = DatabaseManager.shared.getAllItems()
+    }
+
+    private func addItem() {
+        DatabaseManager.shared.addItem(name: itemName)
+        itemName = ""
+        fetchItems()
+    }
+
+    private func deleteItem(name: String) {
+        DatabaseManager.shared.deleteItem(name: name)
+        fetchItems()
+    }
+
 }
 
 struct HomeView_Previews: PreviewProvider {
