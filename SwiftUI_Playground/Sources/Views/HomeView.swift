3d24e4fb17a9df9560c78b0cb0a5aee388027eae
Masaya1582 <masa8y88@gmail.com>
2023-07-23 11:23:08 +0900

    Type Castingを学ぶ

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,22 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
+    private let pokemons: [Pokemon] = [
+        RegularPokemon(name: "Bulbasaur", type: "Grass", ability: "Overgrow - Powers up Grass-type moves in a pinch."),
+        RegularPokemon(name: "Charmander", type: "Fire", ability: "Blaze - Powers up Fire-type moves in a pinch."),
+        LegendaryPokemon(name: "Articuno", type: "Ice/Flying", description: "A legendary bird Pokémon that can control ice."),
+        LegendaryPokemon(name: "Mewtwo", type: "Psychic", description: "It was created based on the DNA of a Pokémon called Mew.")
+    ]
 
     var body: some View {
-        VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 200)
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            Spacer().frame(height: 100)
+        NavigationView {
+            PokemonListView(pokemons: pokemons)
         }
     }
 }
