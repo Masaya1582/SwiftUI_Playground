df6b9b3acf0d0413c2516c7006d9057f9608d067
Masaya1582 <masa8y88@gmail.com>
2023-07-29 12:55:45 +0900

    Dateを深く学ぶ

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,23 +8,62 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
+    @State private var selectedDate = Date()
 
     var body: some View {
         VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 200)
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            Spacer().frame(height: 100)
+            DatePicker("⏰", selection: $selectedDate, displayedComponents: [.date])
+                .datePickerStyle(WheelDatePickerStyle())
+
+            Text("Selected Date: \(formatDate(selectedDate))")
+                .padding()
+
+            Divider()
+
+            Text("Information About Selected Date:")
+                .font(.headline)
+
+            Text("Weekday: \(weekday(selectedDate))")
+                .padding()
+
+            Text("Time Difference from Today:\n \(timeDifference(from: Date(), to: selectedDate))")
+                .padding()
+
+            Spacer()
         }
+        .padding()
+    }
+
+    private func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        return dateFormatter.string(from: date)
+    }
+
+    private func weekday(_ date: Date) -> String {
+        let calendar = Calendar.current
+        let weekdayNumber = calendar.component(.weekday, from: date)
+        let weekdayNames = calendar.weekdaySymbols
+        return weekdayNames[weekdayNumber - 1]
+    }
+
+    private func timeDifference(from startDate: Date, to endDate: Date) -> String {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day, .hour, .minute], from: startDate, to: endDate)
+
+        var timeDifferenceString = ""
+        if let days = components.day, days > 0 {
+            timeDifferenceString += "\(days) day(s) "
+        }
+        if let hours = components.hour, hours > 0 {
+            timeDifferenceString += "\(hours) hour(s) "
+        }
+        if let minutes = components.minute {
+            timeDifferenceString += "\(minutes) minute(s)"
+        }
+
+        return timeDifferenceString
     }
 }
 
