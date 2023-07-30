741a49dea6a9ccf847c94ebe08bc39486eae97d2
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-31 08:13:41 +0900

    Create Account処理追加

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -11,6 +11,7 @@ import FirebaseAuth
 struct HomeView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var userName = ""
     @State private var errorMessage = ""
     @State private var isLoginSuccessful = false
 
@@ -24,12 +25,23 @@ struct HomeView: View {
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
 
+            TextField("UserName", text: $userName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
             Button {
                 signIn()
             } label: {
                 Text("Login")
             }
             .modifier(ButtonModifier(foregroundColor: .white, backgroundColor: .orange))
+
+            Button {
+                createAccount()
+            } label: {
+                Text("Create Account")
+            }
+            .modifier(ButtonModifier(foregroundColor: .white, backgroundColor: .green))
 
             Text(errorMessage)
                 .foregroundColor(.red)
@@ -46,6 +58,32 @@ struct HomeView: View {
                 errorMessage = error.localizedDescription
             } else {
                 isLoginSuccessful = true
+            }
+        }
+    }
+
+    private func createAccount() {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("DEBUG_PRINT: " + error.localizedDescription)
+                return
+            }
+            print("DEBUG_PRINT: ユーザー作成に成功しました。")
+            // 表示名を設定する
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = userName
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        // プロフィールの更新でエラーが発生
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    guard let userName = user.displayName else { return }
+                    print("DEBUG_PRINT: [displayName = \(userName)]の設定に成功しました。")
+                    // self.dismiss(animated: true, completion: nil)
+                }
             }
         }
     }
