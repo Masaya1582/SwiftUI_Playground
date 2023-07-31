87094ddcb0f907299990e2645043cac7062b9ede
Masaya Nakakuki <masa8y88@gmail.com>
2023-08-01 07:50:56 +0900

    onHoverの使い方

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,23 +8,15 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
+    @State private var overText = false
 
     var body: some View {
-        VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 200)
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            Spacer().frame(height: 100)
-        }
+        Text("Hello, World!")
+            .foregroundStyle(overText ? .green : .red)
+            .font(.custom(FontFamily.Caprasimo.regular, size: 24))
+            .onHover { over in
+                overText = over
+            }
     }
 }
 
