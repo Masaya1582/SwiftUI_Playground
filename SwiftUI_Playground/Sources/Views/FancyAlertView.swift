55097a1ddce05cacf31ae218879d0ddfdd94d7b4
Masaya1582 <masa8y88@gmail.com>
2023-09-10 12:17:06 +0900

    FancyAlertView

---
+++ SwiftUI_Playground/Sources/Views/FancyAlertView.swift
@@ -0,0 +1,49 @@
+//
+//  FancyAlertView.swift
+//  SwiftUI_Playground
+//
+//  Created by MasayaNakakuki on 2023/09/10.
+//
+
+import SwiftUI
+
+struct FancyAlertView: View {
+    @Binding var isShowing: Bool
+
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 20) {
+                Text("Fancy Alert")
+                    .font(.title)
+                    .foregroundColor(.white)
+
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.yellow)
+
+                Text("This is a fancy alert with SwiftUI.")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+
+                Button(action: {
+                    isShowing = false
+                }) {
+                    Text("Dismiss")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+            .background(Color.black)
+            .cornerRadius(20)
+        }
+        .opacity(isShowing ? 1 : 0)
+        .animation(.easeInOut(duration: 0.3))
+    }
+}
--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
+    @State private var showAlert = false
 
     var body: some View {
         VStack {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 200)
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            Spacer().frame(height: 100)
+            Button {
+                self.showAlert.toggle()
+            } label: {
+                Text("Show Fancy Alert")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .sheet(isPresented: $showAlert) {
+                FancyAlertView(isShowing: $showAlert)
+            }
         }
     }
 }
