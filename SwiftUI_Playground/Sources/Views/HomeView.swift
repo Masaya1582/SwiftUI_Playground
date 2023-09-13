bfea0fc75f4400c61c130f5f8b1908ee7fa2af6d
Masaya Nakakuki <masa8y88@gmail.com>
2023-09-13 13:14:42 +0900

    Add Button to switch Color

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -9,21 +9,40 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject var viewModel = HomeViewModel()
+    @State private var shouldInvertColor = false
 
     var body: some View {
         VStack {
             Text("Dio")
                 .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .colorInvert()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 200)
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.black, lineWidth: 2)
-                )
+            if shouldInvertColor {
+                Asset.Assets.imgDio.swiftUIImage
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    .colorInvert()
+            } else {
+                Asset.Assets.imgDio.swiftUIImage
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+            }
+            Button {
+                shouldInvertColor.toggle()
+            } label: {
+                Text(shouldInvertColor ? "Revert Color" : "Invert Color")
+                    .modifier(ButtonModifier(foregroundColor: .white, backgroundColor: .orange))
+            }
             Spacer().frame(height: 100)
         }
     }
