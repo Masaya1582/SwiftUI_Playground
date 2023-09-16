361df1876c10d82c8d8d66f909bc43e10947890e
Masaya1582 <masa8y88@gmail.com>
2023-09-16 18:15:31 +0900

    rotationEffectを学ぶ

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,42 +8,42 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
-    @State private var shouldInvertColor = false
+    @State private var rotationAngle: Angle = .zero
 
     var body: some View {
-        VStack(spacing: 28) {
-            Text("Dio")
-                .font(.custom(FontFamily.Caprasimo.regular, size: 42))
-            if shouldInvertColor {
-                Asset.Assets.imgDio.swiftUIImage
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200, height: 200)
-                    .clipShape(Circle())
-                    .colorInvert()
-                    .overlay(
-                        Circle()
-                            .stroke(Color.black, lineWidth: 2)
-                    )
-            } else {
-                Asset.Assets.imgDio.swiftUIImage
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200, height: 200)
-                    .clipShape(Circle())
-                    .overlay(
-                        Circle()
-                            .stroke(Color.black, lineWidth: 2)
-                    )
-            }
-            Button {
-                shouldInvertColor.toggle()
-            } label: {
-                Text(shouldInvertColor ? "Revert Color" : "Invert Color")
-                    .modifier(ButtonModifier(foregroundColor: .white, backgroundColor: .orange))
-            }
+        VStack {
+            Text("Rotate the Square")
+                .font(.largeTitle)
+                .padding()
+
+            RotatingSquareView()
+                .padding()
+
+            Text("Pinch to Rotate")
+                .font(.headline)
+                .foregroundColor(.gray)
         }
+    }
+}
+
+struct RotatingSquareView: View {
+    @State private var rotationAngle: Angle = .zero
+
+    var body: some View {
+        Rectangle()
+            .frame(width: 200, height: 200)
+            .foregroundColor(Color.blue)
+            .cornerRadius(20)
+            .rotationEffect(rotationAngle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { angle in
+                        rotationAngle = angle
+                    }
+                    .onEnded { _ in
+                        // Optionally, you can add logic when the gesture ends
+                    }
+            )
     }
 }
 
