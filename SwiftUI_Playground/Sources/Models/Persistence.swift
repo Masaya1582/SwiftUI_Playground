7da7ddb2afd3b6f8214e133a7455c06039df5230
Masaya Nakakuki <masa8y88@gmail.com>
2023-09-20 08:19:05 +0900

    Add Comments

--- SwiftUI_Playground/Sources/Models/Persistence.swift
+++ SwiftUI_Playground/Sources/Models/Persistence.swift
@@ -7,29 +7,39 @@
 
 import CoreData
 
+// Core Data stackの作成、管理を行う.
 struct PersistenceController {
+    // アプリ全体で使えるShared Instance.
     static let shared = PersistenceController()
 
+    // プレビュー、テスト用
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
+
+        // サンプルを10個 in-memoryストアに保存.
         for _ in 0..<10 {
             let newItem = Task(context: viewContext)
             newItem.name = "Sample Task"
         }
+
         do {
+            // 変更を保存.
             try viewContext.save()
         } catch {
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
+
         return result
     }()
 
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
+        // コンテナの作成
         container = NSPersistentContainer(name: "CoreDataSample")
+
         if inMemory {
             if let firstStoreDescription = container.persistentStoreDescriptions.first {
                 firstStoreDescription.url = URL(fileURLWithPath: "/dev/null")
@@ -37,6 +47,8 @@ struct PersistenceController {
                 fatalError("Unable to access persistent store description")
             }
         }
+
+        // ロード
         container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
