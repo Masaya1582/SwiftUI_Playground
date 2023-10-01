115d26809400a59ba97c9c47112a7693752030db
Masaya1582 <masa8y88@gmail.com>
2023-10-01 12:44:41 +0900

    Add Food Pics

--- SwiftUI_Playground/Generated/Assets.swift
+++ SwiftUI_Playground/Generated/Assets.swift
@@ -26,6 +26,11 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 internal enum Asset {
   internal enum Assets {
     internal static let accentColor = ColorAsset(name: "AccentColor")
+    internal static let burger = ImageAsset(name: "burger")
+    internal static let nuggets = ImageAsset(name: "nuggets")
+    internal static let pancakes = ImageAsset(name: "pancakes")
+    internal static let pizza = ImageAsset(name: "pizza")
+    internal static let spaghetti = ImageAsset(name: "spaghetti")
     internal static let imgBarack = ImageAsset(name: "img_barack")
     internal static let imgBiden = ImageAsset(name: "img_biden")
     internal static let imgBill = ImageAsset(name: "img_bill")
