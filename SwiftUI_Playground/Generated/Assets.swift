6cb873458597859c7ca4ed66fec7487aa6d70970
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-10 09:19:37 +0900

    Food MenyのPicker

--- SwiftUI_Playground/Generated/Assets.swift
+++ SwiftUI_Playground/Generated/Assets.swift
@@ -26,11 +26,11 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 internal enum Asset {
   internal enum Assets {
     internal static let accentColor = ColorAsset(name: "AccentColor")
-    internal static let burger = ImageAsset(name: "burger")
-    internal static let nuggets = ImageAsset(name: "nuggets")
-    internal static let pancakes = ImageAsset(name: "pancakes")
-    internal static let pizza = ImageAsset(name: "pizza")
-    internal static let spaghetti = ImageAsset(name: "spaghetti")
+    internal static let burger = ImageAsset(name: "Burger")
+    internal static let nuggets = ImageAsset(name: "Nuggets")
+    internal static let pancakes = ImageAsset(name: "Pancakes")
+    internal static let pizza = ImageAsset(name: "Pizza")
+    internal static let spaghetti = ImageAsset(name: "Spaghetti")
     internal static let imgBarack = ImageAsset(name: "img_barack")
     internal static let imgBiden = ImageAsset(name: "img_biden")
     internal static let imgBill = ImageAsset(name: "img_bill")
