be612df02f07c28ac76264b86b329220c7ce81a1
Masaya1582 <masa8y88@gmail.com>
2023-10-15 12:41:23 +0900

    CloudとSunのView

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,38 +8,51 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
+    @State private var sunPosition: CGPoint = CGPoint(x: 50, y: 150)
+    @State private var isSunRising = false
 
     var body: some View {
         ZStack {
-            if viewModel.isFloatingViewVisible {
-                FloatingView(dismissAction: {
-                    withAnimation {
-                        viewModel.isFloatingViewVisible = false
-                    }
-                })
-                .transition(.asymmetric(insertion: .opacity, removal: .opacity))
-                .zIndex(1)
-            }
-            VStack(spacing: 28) {
-                Text("Dio said: \(viewModel.name)")
-                    .modifier(CustomLabel(foregroundColor: .black, size: 28))
-                TextField("Messages", text: $viewModel.name)
-                    .modifier(CustomTextField(disableAutoCorrection: true))
-                Asset.Assets.imgDio.swiftUIImage
-                    .resizable()
-                    .modifier(CustomImage(width: 200, height: 200))
-                Button {
-                    withAnimation {
-                        viewModel.isFloatingViewVisible = true
-                    }
-                } label: {
-                    Text("Show Popup View")
-                        .modifier(CustomButton(foregroundColor: .white, backgroundColor: .orange))
-                }
-                CustomCircleView()
+            // Background sky
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .yellow]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+
+            // Sun
+            Image(systemName: "sun.max.fill")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .foregroundColor(.yellow)
+                .offset(x: sunPosition.x, y: sunPosition.y)
+
+            // Cloud
+            CloudView()
+                .offset(x: -80, y: 20)
+        }
+        .onAppear {
+            withAnimation(Animation.easeInOut(duration: 5).repeatForever(autoreverses: true)) {
+                isSunRising.toggle()
             }
         }
+        .onChange(of: isSunRising) { newValue in
+            if newValue {
+                sunPosition = CGPoint(x: 50, y: 150)
+            } else {
+                sunPosition = CGPoint(x: 50, y: 350)
+            }
+        }
+    }
+}
+
+struct CloudView: View {
+    var body: some View {
+        Image(systemName: "cloud.fill")
+            .resizable()
+            .frame(width: 200, height: 100)
+            .foregroundColor(.white)
     }
 }
 
