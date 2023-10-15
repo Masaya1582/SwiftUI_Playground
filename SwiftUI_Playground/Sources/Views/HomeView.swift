35227d3b01b775c7e7fd0de04cda5fb2735f09b0
Masaya1582 <masa8y88@gmail.com>
2023-10-15 12:37:25 +0900

    FlowerPetalView(なんか違う)

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -8,38 +8,33 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HomeViewModel()
+    @State private var isBlossomed = false
 
     var body: some View {
         ZStack {
-            if viewModel.isFloatingViewVisible {
-                FloatingView(dismissAction: {
-                    withAnimation {
-                        viewModel.isFloatingViewVisible = false
-                    }
-                })
-                .transition(.asymmetric(insertion: .opacity, removal: .opacity))
-                .zIndex(1)
-            }
-            VStack(spacing: 28) {
-                Text("Dio said: \(viewModel.name)")
-                    .modifier(CustomLabel(foregroundColor: .black, size: 28))
-                TextField("Messages", text: $viewModel.name)
-                    .modifier(CustomTextField(disableAutoCorrection: true))
-                Asset.Assets.imgDio.swiftUIImage
-                    .resizable()
-                    .modifier(CustomImage(width: 200, height: 200))
-                Button {
-                    withAnimation {
-                        viewModel.isFloatingViewVisible = true
-                    }
-                } label: {
-                    Text("Show Popup View")
-                        .modifier(CustomButton(foregroundColor: .white, backgroundColor: .orange))
-                }
-                CustomCircleView()
+            ForEach(0..<6) { petalIndex in
+                PetalView(petalIndex: petalIndex, isBlossomed: isBlossomed)
             }
         }
+        .onAppear {
+            withAnimation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true)) {
+                self.isBlossomed.toggle()
+            }
+        }
+    }
+}
+
+struct PetalView: View {
+    let petalIndex: Int
+    let isBlossomed: Bool
+
+    var body: some View {
+        let rotationAngle = Angle.degrees(Double(petalIndex) * 60)
+
+        return Capsule()
+            .fill(isBlossomed ? Color.pink : Color.red)
+            .frame(width: 50, height: 100)
+            .rotationEffect(isBlossomed ? rotationAngle : .zero)
     }
 }
 
