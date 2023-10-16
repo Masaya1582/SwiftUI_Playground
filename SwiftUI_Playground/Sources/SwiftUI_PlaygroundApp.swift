614cc02eb277645b511661e7f837275bffac4431
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-16 11:44:20 +0900

    Add Custom Longtap Item

--- SwiftUI_Playground/Sources/SwiftUI_PlaygroundApp.swift
+++ SwiftUI_Playground/Sources/SwiftUI_PlaygroundApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct SwiftUI_PlaygroundApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if shortcutItem.type == "com.run.button" {
+            print("Workoutを開始します")
+        }
+        completionHandler(true)
     }
 }
