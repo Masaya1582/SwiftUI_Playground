42d03ea3a0ef9068897c976fa6a1768eb10212b3
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-21 08:26:58 +0900

    Small Refactor

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -16,6 +16,8 @@ struct HomeView: View {
 
     var body: some View {
         ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
             if viewModel.isFloatingViewVisible {
                 FloatingView(dismissAction: {
                     withAnimation {
@@ -26,31 +28,9 @@ struct HomeView: View {
                 .zIndex(1)
             }
             VStack(spacing: 16) {
-                Text("Today's Quote: \(viewModel.name)")
-                    .modifier(CustomLabel(foregroundColor: .black, size: 28))
-                TextField("Quote", text: $viewModel.name)
-                    .modifier(CustomTextField())
-                if let image = viewModel.selectedImage {
-                    Image(uiImage: image)
-                        .resizable()
-                        .modifier(CustomImage(width: 200, height: 200))
-                } else {
-                    Asset.Assets.imgDio.swiftUIImage
-                        .resizable()
-                        .modifier(CustomImage(width: 200, height: 200))
-                }
-                Button("Show Popup View") {
-                    withAnimation {
-                        viewModel.isFloatingViewVisible = true
-                    }
-                }
-                .modifier(CustomButton(foregroundColor: .white, backgroundColor: .orange))
-                Button("Select an Image") {
-                    withAnimation {
-                        viewModel.showSourceTypeAlert = true
-                    }
-                }
-                .modifier(CustomButton(foregroundColor: .white, backgroundColor: .green))
+                topField()
+                middleField()
+                bottomField()
             }
         }
         .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
@@ -70,6 +50,44 @@ struct HomeView: View {
                 }
             )
         }
+    }
+
+    @ViewBuilder
+    private func topField() -> some View {
+        Text("Today's Quote: \(viewModel.name)")
+            .modifier(CustomLabel(foregroundColor: .black, size: 28))
+        TextField("Quote", text: $viewModel.name)
+            .modifier(CustomTextField())
+    }
+
+    @ViewBuilder
+    private func middleField() -> some View {
+        if let image = viewModel.selectedImage {
+            Image(uiImage: image)
+                .resizable()
+                .modifier(CustomImage(width: 200, height: 200))
+        } else {
+            Asset.Assets.imgDio.swiftUIImage
+                .resizable()
+                .modifier(CustomImage(width: 200, height: 200))
+        }
+    }
+
+    @ViewBuilder
+    private func bottomField() -> some View {
+        Button("Show Popup View") {
+            withAnimation {
+                viewModel.isFloatingViewVisible = true
+            }
+        }
+        .modifier(CustomButton(foregroundColor: .white, backgroundColor: .orange))
+
+        Button("Select an Image") {
+            withAnimation {
+                viewModel.showSourceTypeAlert = true
+            }
+        }
+        .modifier(CustomButton(foregroundColor: .white, backgroundColor: .green))
     }
 }
 
