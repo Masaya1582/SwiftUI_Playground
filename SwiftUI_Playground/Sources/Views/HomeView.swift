104c244bf9fb8039b234c799169d12b742bdcc27
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-21 08:50:14 +0900

    Rebase2

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -69,6 +69,7 @@ struct HomeView: View {
             withAnimation {
                 viewModel.isFloatingViewVisible = true
                 print("Hello World")
+                print("Hello World")
             }
         }
         .modifier(CustomButton(foregroundColor: .white, backgroundColor: .orange))
