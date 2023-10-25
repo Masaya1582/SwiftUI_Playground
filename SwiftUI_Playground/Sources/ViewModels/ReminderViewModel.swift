45ebbe325b951b41a9966af9d97b6695ce9d3bca
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-25 10:19:20 +0900

    UserNotificationsを使ってリマインダーを学ぶ

---
+++ SwiftUI_Playground/Sources/ViewModels/ReminderViewModel.swift
@@ -0,0 +1,45 @@
+// 
+//  ReminderViewModel.swift
+//  SwiftUI_Playground
+//
+//  Created by MasayaNakakuki on 2023/06/26.
+//
+
+import SwiftUI
+import UserNotifications
+
+class ReminderViewModel: ObservableObject {
+    @Published var reminders: [Reminder] = []
+    @Published var notificationStatus: UNAuthorizationStatus = .notDetermined
+
+    init() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                self.notificationStatus = settings.authorizationStatus
+            }
+        }
+    }
+
+    func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            DispatchQueue.main.async {
+                self.notificationStatus = granted ? .authorized : .denied
+            }
+        }
+    }
+
+    func scheduleNotification(for reminder: Reminder) {
+        let content = UNMutableNotificationContent()
+        content.title = "Reminder"
+        content.body = reminder.title
+        content.sound = .default
+
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: reminder.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+
+        let request = UNNotificationRequest(identifier: reminder.id.uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+
+        reminders.append(reminder)
+    }
+}
