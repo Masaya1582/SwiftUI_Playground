ae8247865b7e70c195151a16de47ffc9c1bd8e98
Masaya1582 <masa8y88@gmail.com>
2023-11-11 18:48:13 +0900

    Small Refactor

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -10,16 +10,24 @@ import UIKit
 
 struct HomeView: View {
     @State private var responseData: String?
+    @State private var responseCode: Int?
 
     var body: some View {
-        VStack {
+        VStack(spacing: 28) {
             Button("Make HTTP Request") {
                 fetchData()
             }
             .modifier(CustomButton(foregroundColor: .white, backgroundColor: .green))
             if let responseData = responseData {
                 Text("Response: \(responseData)")
-                    .modifier(CustomLabel(foregroundColor: .black, size: 20))
+                    .modifier(CustomLabel(foregroundColor: .black, size: 24))
+                if let responseCode = responseCode {
+                    Text("Response Code: \(responseCode)")
+                        .modifier(CustomLabel(foregroundColor: .black, size: 24))
+                } else {
+                    Text("Response Code: N/A")
+                        .padding()
+                }
             }
         }
     }
@@ -30,10 +38,14 @@ struct HomeView: View {
             return
         }
 
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
                 print("Error: \(error?.localizedDescription ?? "Unknown error")")
                 return
+            }
+
+            if let httpResponse = response as? HTTPURLResponse {
+                self.responseCode = httpResponse.statusCode
             }
 
             if let decodedResponse = try? JSONDecoder().decode(Todo.self, from: data) {
