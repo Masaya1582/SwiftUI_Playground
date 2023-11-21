9cd07f2ac3bf55f1833891929da2b2d63ea6c291
Masaya1582 <masa8y88@gmail.com>
2023-11-21 19:39:06 +0900

    Fix Copyrights

--- SwiftUI_Playground/Sources/Models/ImagePicker.swift
+++ SwiftUI_Playground/Sources/Models/ImagePicker.swift
@@ -2,8 +2,7 @@
 //  ImagePicker.swift
 //  SwiftUI_Playground
 //
-//  Created by 中久木 雅哉(Nakakuki Masaya) on 2023/10/20.
-//  Copyright (c) 2023 ReNKCHANNEL. All rihgts reserved.
+//  Created by MasayaNakakuki on 2023/11/19.
 //
 
 import UIKit
