851fa7d26c3ce88e07020290bdb8c8b8677b44db
Masaya Nakakuki <masa8y88@gmail.com>
2023-12-12 08:38:46 +0900

    FitnessViewを作る

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -6,102 +6,160 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct HomeView: View {
-    @StateObject private var viewModel = HomeViewModel()
+    let calendar = Calendar.current
+    let today = Date()
 
     var body: some View {
-        ZStack {
-            backgroundField()
-            VStack(spacing: 8) {
-                topField()
-                middleField()
-                bottomField()
-            }
-        }
-        .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
-            ImagePicker(selectedImage: $viewModel.selectedImage, sourceType: viewModel.sourceType ?? .photoLibrary)
-        }
-        .sheet(isPresented: $viewModel.isShowHalfModalView) {
-            HalfModalView(halfModalText: $viewModel.halfModalText, isShowHalfView: $viewModel.isShowHalfModalView)
-                .presentationDetents([.medium])
-        }
-        .alert(isPresented: $viewModel.isShowSourceTypeAlert) {
-            Alert(
-                title: Text("Select SourceType"),
-                message: nil,
-                primaryButton: .default(Text("Camera")) {
-                    viewModel.sourceType = .camera
-                    viewModel.isOpenImagePicker = true
-                },
-                secondaryButton: .default(Text("Library")) {
-                    viewModel.sourceType = .photoLibrary
-                    viewModel.isOpenImagePicker = true
+        TabView {
+            VStack(spacing: 20) {
+                // Weekly calendar
+                HStack(spacing: 8) {
+                    ForEach(0..<7, id: \.self) { index in
+                        let weekdayDate = calendar.date(byAdding: .day, value: index, to: startOfWeek)!
+                        VStack {
+                            Text(weekdayDate, format: .dateTime.weekday(.abbreviated))
+                                .font(.caption)
+                            Text(weekdayDate, format: .dateTime.day())
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
                 }
-            )
-        }
-    }
+                .padding()
+                .background(Color.gray.opacity(0.1))
 
-    @ViewBuilder
-    private func topField() -> some View {
-        Text("Today's Quote: \(viewModel.name)")
-            .modifier(CustomLabel(foregroundColor: .black, size: 28))
-        Text(viewModel.halfModalText)
-            .modifier(CustomLabel(foregroundColor: .black, size: 20))
-        TextField("Quote", text: $viewModel.name)
-            .modifier(CustomTextField())
-    }
+                // Circular progress bar for daily step goal
+                ZStack {
+                    Circle()
+                        .stroke(lineWidth: 20.0)
+                        .opacity(0.3)
+                        .foregroundColor(.blue)
 
-    @ViewBuilder
-    private func middleField() -> some View {
-        if let image = viewModel.selectedImage {
-            Image(uiImage: image)
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        } else {
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        }
-    }
+                    Circle()
+                        .trim(from: 0.0, to: 0.4)
+                        .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
+                        .foregroundColor(.purple)
+                        .rotationEffect(Angle(degrees: 270.0))
 
-    @ViewBuilder
-    private func bottomField() -> some View {
-        Button("Show Popup View") {
-            withAnimation {
-                viewModel.isFloatingViewVisible = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.blue.swiftUIColor))
-
-        Button("Select an Image") {
-            withAnimation {
-                viewModel.isShowSourceTypeAlert = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.alertRed.swiftUIColor))
-
-        Button("Show HalfModalView") {
-            withAnimation {
-                viewModel.isShowHalfModalView = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.black.swiftUIColor))
-    }
-
-    @ViewBuilder
-    private func backgroundField() -> some View {
-        LinearGradient(gradient: Gradient(colors: [Color.purple, Color.red]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.all)
-        if viewModel.isFloatingViewVisible {
-            FloatingView(dismissAction: {
-                withAnimation {
-                    viewModel.isFloatingViewVisible = false
+                    Text("7,235 steps")
+                        .font(.title)
+                        .fontWeight(.bold)
                 }
-            })
-            .transition(.asymmetric(insertion: .opacity, removal: .opacity))
-            .zIndex(1)
+                .frame(width: 200, height: 200)
+                .padding(.vertical, 8)
+
+                // Icons for calories, distance, and active minutes
+                HStack(spacing: 16) {
+                    // Calories burned
+                    Image(systemName: "flame.fill")
+                        .foregroundColor(.red)
+                        .imageScale(.large)
+                        .frame(width: 50, height: 50)
+                        .background(Color.gray.opacity(0.1))
+                        .clipShape(Circle())
+
+                    // Distance covered
+                    Image(systemName: "figure.walk.circle")
+                        .foregroundColor(.blue)
+                        .imageScale(.large)
+                        .frame(width: 50, height: 50)
+                        .background(Color.gray.opacity(0.1))
+                        .clipShape(Circle())
+
+                    // Active minutes
+                    Image(systemName: "clock.fill")
+                        .foregroundColor(.green)
+                        .imageScale(.large)
+                        .frame(width: 50, height: 50)
+                        .background(Color.gray.opacity(0.1))
+                        .clipShape(Circle())
+                }
+                .padding(.vertical, 8)
+
+                // Tabs for Daily, Week, Month
+                HStack {
+                    Text("DAILY")
+                        .foregroundColor(.gray)
+                    Text("WEEK")
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .background(Capsule().fill(Color.purple))
+                    Text("MONTH")
+                        .foregroundColor(.gray)
+                }
+                .font(.headline)
+                .padding(.vertical, 8)
+
+                // Line graph for weekly step count
+                LineGraphView()
+                    .frame(height: 200)
+                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(LinearGradient(
+                                colors: [Color.purple.opacity(0.6), Color.purple],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            ))
+                    )
+            }
+            .tabItem {
+                Label("Home", systemImage: "house")
+            }
+
+            Text("Search")
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+
+            Text("Notifications")
+                .tabItem {
+                    Label("Notifications", systemImage: "bell")
+                }
+
+            Text("Profile")
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+        }
+        .padding()
+    }
+
+    var startOfWeek: Date {
+        let components = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: today)
+        return calendar.date(from: components)!
+    }
+}
+
+struct LineGraphView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            let dataPoints: [CGFloat] = [0.2, 0.5, 0.3, 0.8, 0.7, 0.6, 0.9]
+            let stepHeight = geometry.size.height
+            let stepWidth = geometry.size.width / CGFloat(dataPoints.count - 1)
+
+            Path { path in
+                for index in dataPoints.indices {
+                    let xPosition = stepWidth * CGFloat(index)
+                    let yPosition = stepHeight * (1 - dataPoints[index])
+
+                    if index == 0 {
+                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+                    } else {
+                        path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                    }
+                }
+            }
+            .stroke(Color.white, lineWidth: 2)
+
+            ForEach(dataPoints.indices, id: \.self) { index in
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 8, height: 8)
+                    .position(x: stepWidth * CGFloat(index), y: stepHeight * (1 - dataPoints[index]))
+            }
         }
     }
 }
