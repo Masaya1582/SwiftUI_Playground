71fdf79661058c2301a67815aefadabc4675dc12
Masaya Nakakuki <masa8y88@gmail.com>
2024-01-05 11:23:05 +0900

    Hide Timer after 0

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -13,15 +13,17 @@ struct HomeView: View {
 
     var body: some View {
         VStack {
-            Text(String(count))
-                .modifier(CustomLabel(foregroundColor: .black, size: 48))
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 120)
-                        .foregroundColor(.white)
-                        .frame(width: 200, height: 200)
-                        .shadow(color: .gray, radius: 4, x: 0, y: 2)
-                )
+            if count > 0 {
+                Text(String(count))
+                    .modifier(CustomLabel(foregroundColor: .black, size: 48))
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 120)
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 200)
+                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                    )
+            }
         }
         .onAppear {
             startTimer()
@@ -34,6 +36,10 @@ struct HomeView: View {
     private func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             count -= 1
+            if count < 0 {
+                timer?.invalidate()
+                timer = nil
+            }
         }
     }
 
