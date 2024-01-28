c11aa2bb54a719e9083bfafd141cafd3b41303bc
Masaya1582 <masa8y88@gmail.com>
2024-01-28 22:44:56 +0900

    Rebase Practice!

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -22,6 +22,9 @@ struct HomeView: View {
                 bottomField()
             }
         }
+        .onAppear {
+            print("Hello World")
+        }
         .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
             ImagePicker(selectedImage: $viewModel.selectedImage, sourceType: viewModel.sourceType ?? .photoLibrary)
         }
