0ebbb5639123386a83eda4f4e49d5a541b78f643
Masaya Nakakuki <masa8y88@gmail.com>
2024-03-26 10:13:19 +0900

    Refactor Localizable.strings

--- SwiftUI_Playground/Generated/LocalizableStrings.swift
+++ SwiftUI_Playground/Generated/LocalizableStrings.swift
@@ -11,25 +11,25 @@ import Foundation
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
   /// A rose by any other name would smell as sweet.
-  internal static let sampleText1 = L10n.tr("Localizable", "sample_text1", fallback: "A rose by any other name would smell as sweet.")
+  internal static let defaultText1 = L10n.tr("Localizable", "default_text1", fallback: "A rose by any other name would smell as sweet.")
   /// Genius is one percent inspiration and ninety-nine percent perspiration.
-  internal static let sampleText10 = L10n.tr("Localizable", "sample_text10", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
+  internal static let defaultText10 = L10n.tr("Localizable", "default_text10", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
   /// Ask not what your country can do for you; ask what you can do for your country.
-  internal static let sampleText2 = L10n.tr("Localizable", "sample_text2", fallback: "Ask not what your country can do for you; ask what you can do for your country.")
+  internal static let defaultText2 = L10n.tr("Localizable", "default_text2", fallback: "Ask not what your country can do for you; ask what you can do for your country.")
   /// Ask, and it shall be given you; seek, and you shall find.
-  internal static let sampleText3 = L10n.tr("Localizable", "sample_text3", fallback: "Ask, and it shall be given you; seek, and you shall find.")
+  internal static let defaultText3 = L10n.tr("Localizable", "default_text3", fallback: "Ask, and it shall be given you; seek, and you shall find.")
   /// Genius is one percent inspiration and ninety-nine percent perspiration.
-  internal static let sampleText4 = L10n.tr("Localizable", "sample_text4", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
+  internal static let defaultText4 = L10n.tr("Localizable", "default_text4", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
   /// A rose by any other name would smell as sweet.
-  internal static let sampleText5 = L10n.tr("Localizable", "sample_text5", fallback: "A rose by any other name would smell as sweet.")
+  internal static let defaultText5 = L10n.tr("Localizable", "default_text5", fallback: "A rose by any other name would smell as sweet.")
   /// Ask not what your country can do for you; ask what you can do for your country.
-  internal static let sampleText6 = L10n.tr("Localizable", "sample_text6", fallback: "Ask not what your country can do for you; ask what you can do for your country.")
+  internal static let defaultText6 = L10n.tr("Localizable", "default_text6", fallback: "Ask not what your country can do for you; ask what you can do for your country.")
   /// Ask, and it shall be given you; seek, and you shall find.
-  internal static let sampleText7 = L10n.tr("Localizable", "sample_text7", fallback: "Ask, and it shall be given you; seek, and you shall find.")
+  internal static let defaultText7 = L10n.tr("Localizable", "default_text7", fallback: "Ask, and it shall be given you; seek, and you shall find.")
   /// Genius is one percent inspiration and ninety-nine percent perspiration.
-  internal static let sampleText8 = L10n.tr("Localizable", "sample_text8", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
+  internal static let defaultText8 = L10n.tr("Localizable", "default_text8", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
   /// Genius is one percent inspiration and ninety-nine percent perspiration.
-  internal static let sampleText9 = L10n.tr("Localizable", "sample_text9", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
+  internal static let defaultText9 = L10n.tr("Localizable", "default_text9", fallback: "Genius is one percent inspiration and ninety-nine percent perspiration.")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
