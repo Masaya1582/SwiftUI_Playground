3a2e200710859f9f39312f07f75b1502390adc1d
Masaya Nakakuki <masa8y88@gmail.com>
2024-03-28 09:32:30 +0900

    ForcePushTest1

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -26,7 +26,7 @@ struct HomeView: View {
         .onAppear {
             let randomID = Int.random(in: 1...100)
             pokeAPIManager.fetchPokemon(withID: randomID) { pokemon in
-                print("ポケモンDetail: \(pokemon)")
+                print("ポケモンDetails: \(pokemon)")
             }
         }
         .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
