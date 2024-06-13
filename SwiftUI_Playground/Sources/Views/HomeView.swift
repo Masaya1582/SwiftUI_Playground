7322f8b57a7c0d7b4b51c8002cddaae7219d5d44
Masaya1582 <masa8y88@gmail.com>
2024-06-13 11:18:27 +0900

    Simple Setting View

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -9,109 +9,99 @@ import SwiftUI
 import UIKit
 
 struct HomeView: View {
-    // MARK: - Properties
-    @StateObject private var viewModel = HomeViewModel()
-    private let pokeAPIManager = PokeAPIManager()
-
-    // MARK: - Body
     var body: some View {
-        ZStack {
-            backgroundField()
-            VStack(spacing: 8) {
-                topField()
-                middleField()
-                bottomField()
-            }
-        }
-        .onAppear {
-            let randomID = Int.random(in: 1...100)
-            pokeAPIManager.fetchPokemon(withID: randomID) { pokemon in
-                print("ポケモンDetailsだよ: \(pokemon)")
-            }
-        }
-        .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
-            ImagePicker(selectedImage: $viewModel.selectedImage, sourceType: viewModel.sourceType ?? .photoLibrary)
-        }
-        .sheet(isPresented: $viewModel.isShowHalfModalView) {
-            HalfModalView(halfModalText: $viewModel.halfModalText, isShowHalfView: $viewModel.isShowHalfModalView)
-                .presentationDetents([.medium])
-        }
-        .alert(isPresented: $viewModel.isShowSourceTypeAlert) {
-            Alert(
-                title: Text("Choose SourceType"),
-                message: nil,
-                primaryButton: .default(Text("Camera")) {
-                    viewModel.sourceType = .camera
-                    viewModel.isOpenImagePicker = true
-                },
-                secondaryButton: .default(Text("Library")) {
-                    viewModel.sourceType = .photoLibrary
-                    viewModel.isOpenImagePicker = true
+        NavigationView {
+            List {
+                Section(header: Text("General")) {
+                    NavigationLink(destination: AccountView()) {
+                        HStack {
+                            Image(systemName: "person.circle")
+                            Text("Account")
+                        }
+                    }
+                    NavigationLink(destination: NotificationsView()) {
+                        HStack {
+                            Image(systemName: "bell")
+                            Text("Notifications")
+                        }
+                    }
                 }
-            )
-        }
-    }
 
-    @ViewBuilder
-    private func topField() -> some View {
-        Text("Today's Quote: \(viewModel.name)")
-            .modifier(CustomLabel(foregroundColor: .black, size: 28))
-        Text(viewModel.halfModalText)
-            .modifier(CustomLabel(foregroundColor: .black, size: 20))
-        TextField("Quote", text: $viewModel.name)
-            .modifier(CustomTextField())
-    }
-
-    @ViewBuilder
-    private func middleField() -> some View {
-        if let image = viewModel.selectedImage {
-            Image(uiImage: image)
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        } else {
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        }
-    }
-
-    @ViewBuilder
-    private func bottomField() -> some View {
-        Button("Show Popup View") {
-            withAnimation {
-                viewModel.isFloatingViewVisible = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.blue.swiftUIColor))
-
-        Button("Select an Image") {
-            withAnimation {
-                viewModel.isShowSourceTypeAlert = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.alertRed.swiftUIColor))
-
-        Button("Show HalfModalView") {
-            withAnimation {
-                viewModel.isShowHalfModalView = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.black.swiftUIColor))
-    }
-
-    @ViewBuilder
-    private func backgroundField() -> some View {
-        LinearGradient(gradient: Gradient(colors: [Color.orange, Color.red]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.all)
-        if viewModel.isFloatingViewVisible {
-            FloatingView(dismissAction: {
-                withAnimation {
-                    viewModel.isFloatingViewVisible = false
+                Section(header: Text("Preferences")) {
+                    NavigationLink(destination: AppearanceView()) {
+                        HStack {
+                            Image(systemName: "paintbrush")
+                            Text("Appearance")
+                        }
+                    }
+                    NavigationLink(destination: PrivacyView()) {
+                        HStack {
+                            Image(systemName: "lock")
+                            Text("Privacy")
+                        }
+                    }
                 }
-            })
-            .transition(.asymmetric(insertion: .opacity, removal: .opacity))
-            .zIndex(1)
+
+                Section(header: Text("About")) {
+                    NavigationLink(destination: AboutView()) {
+                        HStack {
+                            Image(systemName: "info.circle")
+                            Text("About Us")
+                        }
+                    }
+                    NavigationLink(destination: HelpView()) {
+                        HStack {
+                            Image(systemName: "questionmark.circle")
+                            Text("Help")
+                        }
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("Settings")
         }
+    }
+}
+
+struct AccountView: View {
+    var body: some View {
+        Text("Account Settings")
+            .navigationBarTitle("Account", displayMode: .inline)
+    }
+}
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications Settings")
+            .navigationBarTitle("Notifications", displayMode: .inline)
+    }
+}
+
+struct AppearanceView: View {
+    var body: some View {
+        Text("Appearance Settings")
+            .navigationBarTitle("Appearance", displayMode: .inline)
+    }
+}
+
+struct PrivacyView: View {
+    var body: some View {
+        Text("Privacy Settings")
+            .navigationBarTitle("Privacy", displayMode: .inline)
+    }
+}
+
+struct AboutView: View {
+    var body: some View {
+        Text("About Us")
+            .navigationBarTitle("About Us", displayMode: .inline)
+    }
+}
+
+struct HelpView: View {
+    var body: some View {
+        Text("Help")
+            .navigationBarTitle("Help", displayMode: .inline)
     }
 }
 
