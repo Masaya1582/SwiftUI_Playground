d5f39b091e91b1bcaeeb97b01007fb6be7de0ca3
Masaya1582 <masa8y88@gmail.com>
2024-07-16 09:23:06 +0900

    カスタムTabView

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -6,112 +6,217 @@
 //
 
 import SwiftUI
-import UIKit
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case services = "Services"
+    case partners = "Partners"
+    case activity = "Activity"
+
+    var systemImage: String {
+        switch self {
+        case .home:
+            return "house"
+        case .services:
+            return "envelope.open.badge.clock"
+        case .partners:
+            return "hand.raised"
+        case .activity:
+            return "bell"
+        }
+    }
+
+    var index: Int {
+        return Tab.allCases.firstIndex(of: self) ?? 0
+    }
+}
+
+struct PositionKey: PreferenceKey {
+    static var defaultValue: CGRect = .zero
+
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func viewPosition(completion: @escaping (CGRect) -> ()) -> some View {
+        self
+            .overlay {
+                GeometryReader {
+                    let rect = $0.frame(in: .global)
+
+                    Color.clear
+                        .preference(key: PositionKey.self, value: rect)
+                        .onPreferenceChange(PositionKey.self, perform: completion)
+                }
+            }
+    }
+}
+
+struct TabShape: Shape {
+    var midpoint: CGFloat
+
+    /// Adding Shape Animation
+    var animatableData: CGFloat {
+        get { midpoint }
+        set {
+            midpoint = newValue
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            /// First Drawing the Rectangle Shape
+            path.addPath(Rectangle().path(in: rect))
+            /// Now Drawing Upward Curve Shape
+            path.move(to: .init(x: midpoint - 60, y: 0))
+
+            let to = CGPoint(x: midpoint, y: -25)
+            let control1 = CGPoint(x: midpoint - 30, y: 0)
+            let control2 = CGPoint(x: midpoint - 30, y: -25)
+
+            path.addCurve(to: to, control1: control1, control2: control2)
+
+            let to1 = CGPoint(x: midpoint + 60, y: 0)
+            let control3 = CGPoint(x: midpoint + 30, y: -25)
+            let control4 = CGPoint(x: midpoint + 30, y: 0)
+
+            path.addCurve(to: to1, control1: control3, control2: control4)
+        }
+    }
+}
+
+struct Home: View {
+    /// View Properties
+    @State private var activeTab: Tab = .home
+    /// For Smooth Shape Sliding Effect, We're going to use Matched Geometry Effect
+    @Namespace private var animation
+    @State private var tabShapePosition: CGPoint = .zero
+    init() {
+        /// Hiding Tab Bar Due To SwiftUI iOS 16.4 Bug
+        UITabBar.appearance().isHidden = true
+    }
+    var body: some View {
+        VStack(spacing: 0) {
+            TabView(selection: $activeTab) {
+                Text("Home")
+                    .tag(Tab.home)
+                /// Hiding Native Tab Bar
+                /// Bug on iOS 16.4
+                // .toolbar(.hidden, for: .tabBar)
+
+                Text("Services")
+                    .tag(Tab.services)
+                /// Hiding Native Tab Bar
+                /// Bug on iOS 16.4
+                // .toolbar(.hidden, for: .tabBar)
+
+                Text("Partners")
+                    .tag(Tab.partners)
+                /// Hiding Native Tab Bar
+                /// Bug on iOS 16.4
+                // .toolbar(.hidden, for: .tabBar)
+
+                Text("Activity")
+                    .tag(Tab.activity)
+                /// Hiding Native Tab Bar
+                /// Bug on iOS 16.4
+                // .toolbar(.hidden, for: .tabBar)
+            }
+
+            customTabBar()
+        }
+    }
+
+    /// Custom Tab Bar
+    /// With More Easy Customization
+    @ViewBuilder
+    func customTabBar(_ tint: Color = .blue, _ inactiveTint: Color = .blue) -> some View {
+        /// Moving all the Remaining Tab Item's to Bottom
+        HStack(alignment: .bottom, spacing: 0) {
+            ForEach(Tab.allCases, id: \.rawValue) {
+                TabItem(
+                    tint: tint,
+                    inactiveTint: inactiveTint,
+                    tab: $0,
+                    animation: animation,
+                    activeTab: $activeTab,
+                    position: $tabShapePosition
+                )
+            }
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 10)
+        .background(content: {
+            TabShape(midpoint: tabShapePosition.x)
+                .fill(.white)
+                .ignoresSafeArea()
+            /// Adding Blur + Shadow
+            /// For Shape Smoothening
+                .shadow(color: tint.opacity(0.2), radius: 5, x: 0, y: -5)
+                .blur(radius: 2)
+                .padding(.top, 25)
+        })
+        /// Adding Animation
+        .animation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7), value: activeTab)
+    }
+}
+
+/// Tab Bar Item
+struct TabItem: View {
+    var tint: Color
+    var inactiveTint: Color
+    var tab: Tab
+    var animation: Namespace.ID
+    @Binding var activeTab: Tab
+    @Binding var position: CGPoint
+
+    /// Each Tab Item Position on the Screen
+    @State private var tabPosition: CGPoint = .zero
+    var body: some View {
+        VStack(spacing: 5) {
+            Image(systemName: tab.systemImage)
+                .font(.title2)
+                .foregroundColor(activeTab == tab ? .white : inactiveTint)
+            /// Increasing Size for the Active Tab
+                .frame(width: activeTab == tab ? 58 : 35, height: activeTab == tab ? 58 : 35)
+                .background {
+                    if activeTab == tab {
+                        Circle()
+                            .fill(tint.gradient)
+                            .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+                    }
+                }
+
+            Text(tab.rawValue)
+                .font(.caption)
+                .foregroundColor(activeTab == tab ? tint : .gray)
+        }
+        .frame(maxWidth: .infinity)
+        .contentShape(Rectangle())
+        .viewPosition(completion: { rect in
+            tabPosition.x = rect.midX
+
+            /// Updating Active Tab Position
+            if activeTab == tab {
+                position.x = rect.midX
+            }
+        })
+        .onTapGesture {
+            activeTab = tab
+
+            withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                position.x = tabPosition.x
+            }
+        }
+    }
+}
 
 struct HomeView: View {
-    // MARK: - Properties
-    @StateObject private var viewModel = HomeViewModel()
-    private let pokeAPIManager = PokeAPIManager()
-
-    // MARK: - Body
     var body: some View {
-        ZStack {
-            backgroundField()
-            VStack(spacing: 8) {
-                topField()
-                middleField()
-                bottomField()
-            }
-        }
-        .onAppear {
-            let randomID = Int.random(in: 1...100)
-            pokeAPIManager.fetchPokemon(withID: randomID) { pokemon in
-                print("ポケモンDetailsだよ: \(pokemon)")
-            }
-        }
-        .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
-            ImagePicker(selectedImage: $viewModel.selectedImage, sourceType: viewModel.sourceType ?? .photoLibrary)
-        }
-        .sheet(isPresented: $viewModel.isShowHalfModalView) {
-            HalfModalView(halfModalText: $viewModel.halfModalText, isShowHalfView: $viewModel.isShowHalfModalView)
-                .presentationDetents([.medium])
-        }
-        .alert(isPresented: $viewModel.isShowSourceTypeAlert) {
-            Alert(
-                title: Text("Choose SourceType"),
-                message: nil,
-                primaryButton: .default(Text("Camera")) {
-                    viewModel.sourceType = .camera
-                    viewModel.isOpenImagePicker = true
-                },
-                secondaryButton: .default(Text("Library")) {
-                    viewModel.sourceType = .photoLibrary
-                    viewModel.isOpenImagePicker = true
-                }
-            )
-        }
-    }
-
-    @ViewBuilder
-    private func topField() -> some View {
-        Text("Today's Quote: \(viewModel.name)")
-            .modifier(CustomLabel(foregroundColor: .black, size: 28))
-        Text(viewModel.halfModalText)
-            .modifier(CustomLabel(foregroundColor: .black, size: 20))
-        TextField("Quote", text: $viewModel.name)
-            .modifier(CustomTextField())
-    }
-
-    @ViewBuilder
-    private func middleField() -> some View {
-        if let image = viewModel.selectedImage {
-            Image(uiImage: image)
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        } else {
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        }
-    }
-
-    @ViewBuilder
-    private func bottomField() -> some View {
-        Button("Show Popup View") {
-            withAnimation {
-                viewModel.isFloatingViewVisible = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.blue.swiftUIColor))
-
-        Button("Select an Image") {
-            withAnimation {
-                viewModel.isShowSourceTypeAlert = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.alertRed.swiftUIColor))
-
-        Button("Show HalfModalView") {
-            withAnimation {
-                viewModel.isShowHalfModalView = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.black.swiftUIColor))
-    }
-
-    @ViewBuilder
-    private func backgroundField() -> some View {
-        LinearGradient(gradient: Gradient(colors: [Color.orange, Color.red]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.all)
-        if viewModel.isFloatingViewVisible {
-            FloatingView(dismissAction: {
-                withAnimation {
-                    viewModel.isFloatingViewVisible = false
-                }
-            })
-            .transition(.asymmetric(insertion: .opacity, removal: .opacity))
-            .zIndex(1)
-        }
+        Home()
     }
 }
 
