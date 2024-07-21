ad0db6b870afac3f5cc3a0063096c7527cf3d32e
Masaya1582 <masa8y88@gmail.com>
2024-07-21 10:38:05 +0900

    ShopKartView

--- SwiftUI_Playground/Sources/Views/HomeView.swift
+++ SwiftUI_Playground/Sources/Views/HomeView.swift
@@ -6,111 +6,251 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct HomeView: View {
-    // MARK: - Properties
-    @StateObject private var viewModel = HomeViewModel()
-    private let pokeAPIManager = PokeAPIManager()
+    var body: some View {
+        ShopUI()
+    }
+}
 
-    // MARK: - Body
+struct ShopUI: View {
     var body: some View {
         ZStack {
-            backgroundField()
-            VStack(spacing: 8) {
-                topField()
-                middleField()
-                bottomField()
+            Color.white.edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                WatchBody()
+                    .offset(x: 0, y: 20)
+                Spacer()
+                ShopOptions()
             }
         }
-        .onAppear {
-            let randomID = Int.random(in: 1...100)
-            pokeAPIManager.fetchPokemon(withID: randomID) { pokemon in
-                print("PokemoDetail: \(pokemon)")
-            }
-        }
-        .fullScreenCover(isPresented: $viewModel.isOpenImagePicker) {
-            ImagePicker(selectedImage: $viewModel.selectedImage, sourceType: viewModel.sourceType ?? .photoLibrary)
-        }
-        .sheet(isPresented: $viewModel.isShowHalfModalView) {
-            HalfModalView(halfModalText: $viewModel.halfModalText, isShowHalfView: $viewModel.isShowHalfModalView)
-                .presentationDetents([.medium])
-        }
-        .alert(isPresented: $viewModel.isShowSourceTypeAlert) {
-            Alert(
-                title: Text("Choose SourceType"),
-                message: nil,
-                primaryButton: .default(Text("Camera")) {
-                    viewModel.sourceType = .camera
-                    viewModel.isOpenImagePicker = true
-                },
-                secondaryButton: .default(Text("Library")) {
-                    viewModel.sourceType = .photoLibrary
-                    viewModel.isOpenImagePicker = true
+    }
+}
+
+struct WatchBody: View {
+    @State private var orChair = false
+    @State private var orChairAnim = false
+    @State private var blChair = false
+    @State private var blChairAnim = false
+    @State private var ylChair = false
+    @State private var ylChairAnim = false
+
+    var body: some View {
+        HStack {
+            ZStack {
+                Ellipse()
+                    .frame(width: 300, height: 50)
+                    .offset(y: 160)
+                    .blur(radius: 20)
+                    .opacity(0.5)
+
+                Image("chair")
+                    .resizable()
+                    .frame(width: 340, height: 400)
+                    .contentShape(Rectangle())
+
+                if orChair {
+                    ZStack {
+                        Image("orchair")
+                            .resizable()
+                            .frame(width: 340, height: 400)
+                    }
+                    .opacity(orChairAnim ? 1 : 0)
+                    .animation(Animation.easeInOut(duration: 1).repeatCount(1))
+                    .onAppear {
+                        orChairAnim.toggle()
+                    }
                 }
-            )
-        }
-    }
 
-    @ViewBuilder
-    private func topField() -> some View {
-        Text("Tomorrow's Quote: \(viewModel.name)")
-            .modifier(CustomLabel(foregroundColor: .black, size: 28))
-        Text(viewModel.halfModalText)
-            .modifier(CustomLabel(foregroundColor: .black, size: 20))
-        TextField("Quote", text: $viewModel.name)
-            .modifier(CustomTextField())
-    }
-
-    @ViewBuilder
-    private func middleField() -> some View {
-        if let image = viewModel.selectedImage {
-            Image(uiImage: image)
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        } else {
-            Asset.Assets.imgDio.swiftUIImage
-                .resizable()
-                .modifier(CustomImage(width: 200, height: 200))
-        }
-    }
-
-    @ViewBuilder
-    private func bottomField() -> some View {
-        Button("Show PopupView") {
-            withAnimation {
-                viewModel.isFloatingViewVisible = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.blue.swiftUIColor))
-
-        Button("Select an Image") {
-            withAnimation {
-                viewModel.isShowSourceTypeAlert = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.alertRed.swiftUIColor))
-
-        Button("Show HalfModalView") {
-            withAnimation {
-                viewModel.isShowHalfModalView = true
-            }
-        }
-        .modifier(CustomButton(foregroundColor: .white, backgroundColor: Asset.Colors.black.swiftUIColor))
-    }
-
-    @ViewBuilder
-    private func backgroundField() -> some View {
-        LinearGradient(gradient: Gradient(colors: [Color.orange, Color.red]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.all)
-        if viewModel.isFloatingViewVisible {
-            FloatingView(dismissAction: {
-                withAnimation {
-                    viewModel.isFloatingViewVisible = false
+                if blChair {
+                    ZStack {
+                        Image("blchair")
+                            .resizable()
+                            .frame(width: 340, height: 400)
+                    }
+                    .opacity(blChairAnim ? 1 : 0)
+                    .animation(Animation.easeInOut(duration: 1).repeatCount(1))
+                    .onAppear {
+                        blChairAnim.toggle()
+                    }
                 }
-            })
-            .transition(.asymmetric(insertion: .opacity, removal: .opacity))
-            .zIndex(1)
+
+                if ylChair {
+                    ZStack {
+                        Image("ylchair")
+                            .resizable()
+                            .frame(width: 340, height: 400)
+                    }
+                    .opacity(ylChairAnim ? 1 : 0)
+                    .animation(Animation.easeInOut(duration: 1).repeatCount(1))
+                    .onAppear {
+                        ylChairAnim.toggle()
+                    }
+                }
+            }
+
+            VStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(Color("or"))
+                    .gesture(
+                        TapGesture()
+                            .onEnded {
+                                orChair.toggle()
+                            }
+                    )
+
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(Color("bl"))
+                    .gesture(
+                        TapGesture()
+                            .onEnded {
+                                blChair.toggle()
+                            }
+                    )
+
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(Color("yl"))
+                    .gesture(
+                        TapGesture()
+                            .onEnded {
+                                ylChair.toggle()
+                            }
+                    )
+            }
+            .offset(x: -20, y: 0)
+        }
+    }
+}
+
+struct ShopOptions: View {
+    @State private var heartTap = false
+
+    let para = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt et metus mattis auctor."
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(width: 430, height: 350)
+                .offset(x: 0, y: 55)
+                .foregroundColor(.black)
+
+            VStack {
+                HStack {
+                    Text("Fancy Chair")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .offset(x: 0, y: 90)
+
+                    Spacer()
+
+                    ZStack {
+                        Image(systemName: "heart")
+                            .resizable()
+                            .frame(width: 30, height: 28)
+                            .foregroundColor(.white)
+                            .gesture(
+                                TapGesture()
+                                    .onEnded {
+                                        heartTap.toggle()
+                                    }
+                            )
+
+                        if heartTap {
+                            Image(systemName: "heart.fill")
+                                .resizable()
+                                .frame(width: 30, height: 28)
+                                .foregroundColor(.red)
+                        }
+                    }
+                    .offset(x: 0, y: 90)
+                }
+                .padding(21)
+
+                Spacer().frame(height: 60)
+                Text(para)
+                    .font(.title3)
+                    .fontWeight(.thin)
+                    .foregroundColor(.white.opacity(0.5))
+                    .padding()
+                    .offset(x: 0, y: 25)
+
+                Spacer()
+                ZStack {
+                    ButtonV()
+                    KartView()
+                        .offset(x: -80, y: -40)
+                }
+                .offset(x: 20, y: 50)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ButtonV: View {
+    @State private var arrowAnim = false
+
+    var body: some View {
+        ZStack {
+            HStack {
+                Text("350 $")
+                    .font(.system(size: 30))
+                    .fontWeight(.thin)
+                    .foregroundColor(.white)
+
+                Image(systemName: "chevron.right")
+                    .resizable()
+                    .frame(width: 15, height: 25)
+                    .foregroundColor(.white)
+                    .opacity(arrowAnim ? 0 : 1)
+                    .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true))
+                    .onAppear {
+                        arrowAnim.toggle()
+                    }
+            }
+        }
+        .offset(x: 105, y: -40)
+    }
+}
+
+struct KartView: View {
+    @State private var cartCount = 0
+
+    var body: some View {
+        HStack {
+            Button(action: {
+                cartCount -= 1
+            }) {
+                Text("-")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.white.opacity(0.1))
+                    .cornerRadius(5)
+            }
+
+            Text("\(cartCount)")
+                .font(.system(size: 25))
+                .foregroundColor(.white)
+                .padding()
+
+            Button(action: {
+                cartCount += 1
+            }) {
+                Text("+")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.white.opacity(0.1))
+                    .cornerRadius(5)
+            }
+
+            Spacer()
+                .frame(width: 100)
         }
     }
 }
