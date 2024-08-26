15b1136b2f403326479edcd10a96c67212d57f90
Masaya1582 <masa8y88@gmail.com>
2024-08-26 17:46:39 +0900

    API Requestをポケモンで学ぶPart5くらい？

--- SwiftUI_Playground/Sources/Models/HomeModel.swift
+++ SwiftUI_Playground/Sources/Models/HomeModel.swift
@@ -7,35 +7,17 @@
 
 import Foundation
 
-final class HomeModel {
-    var name: String
-    var age: Int
-    var height: Double
-    var isMan: Bool
-
-    init(name: String, age: Int, height: Double, isMen: Bool) {
-        self.name = name
-        self.age = age
-        self.height = height
-        self.isMan = isMen
-    }
+struct PokemonResponse: Decodable {
+    let results: [Pokemon]
 }
 
-struct Post: Codable {
-    let id: Int
-    let userId: Int
-    let title: String
-    let body: String
-
-    private enum CodingKeys: CodingKey {
-        case id
-        case userId
-        case title
-        case body
-    }
-}
-
-struct Pokemon: Codable {
-    let id: Int
+struct Pokemon: Decodable, Identifiable {
+    let id = UUID()
     let name: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case url
+    }
 }
