21d3031cc1279761309b691d5854d9aa176d5606
Masaya1582 <masa8y88@gmail.com>
2024-09-14 20:12:13 +0900

    Allow Remote Notifications

--- SwiftUI_Playground/Sources/AppDelegate.swift
+++ SwiftUI_Playground/Sources/AppDelegate.swift
@@ -17,8 +17,49 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         return config
     }
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        // 通知の権限をリクエスト
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("Error requesting notifications authorization: \(error)")
+            }
+            // メインスレッドでリモート通知を登録
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+        Messaging.messaging().delegate = self
         return true
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // アプリがフォアグラウンドにいる間に通知を受信した際に呼ばれるメソッド
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
+
+    // 通知をタップした際に呼ばれるメソッド
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
+
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+    // デバイストークンが正常に登録された場合に呼ばれるメソッド
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // APNsトークンをFirebase Messagingに渡す
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    // Firebase MessagingでFCMトークンを受信した際に呼ばれるメソッド
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let token = fcmToken {
+            print("FCM Token: \(token)")
+        }
     }
 }
