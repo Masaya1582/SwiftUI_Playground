b3a1aa3213700e8fe3aeb6da2874a7472b78f38b
Masaya1582 <masa8y88@gmail.com>
2024-09-19 09:12:24 +0900

    Small Refactoring

--- SwiftUI_Playground/Sources/Views/UserDetailView.swift
+++ SwiftUI_Playground/Sources/Views/UserDetailView.swift
@@ -15,121 +15,15 @@ struct UserDetailView: View {
             Form {
                 if let user = userViewModel.users {
                     // User Info Section
-                    Section(header: Text("User Information")) {
-                        HStack {
-                            Text("ID:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text("\(user.id)")
-                        }
-
-                        HStack {
-                            Text("Name:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.name)
-                        }
-
-                        HStack {
-                            Text("Username:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.userName)
-                        }
-
-                        HStack {
-                            Text("Email:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.email)
-                        }
-                    }
-
+                    userInformationField(user)
                     // Address Section
-                    Section(header: Text("Address")) {
-                        HStack {
-                            Text("Street:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.address.street)
-                        }
-
-                        HStack {
-                            Text("Suite:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.address.suite)
-                        }
-
-                        HStack {
-                            Text("City:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.address.city)
-                        }
-
-                        HStack {
-                            Text("Zipcode:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.address.zipcode)
-                        }
-
-                        HStack {
-                            Text("Geo Coordinates:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text("Lat \(user.address.geo.latitude), Lng \(user.address.geo.longitude)")
-                        }
-                    }
-
+                    addressField(user)
                     // Company Section
-                    Section(header: Text("Company Information")) {
-                        HStack {
-                            Text("Company Name:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.company.name)
-                        }
-
-                        HStack {
-                            Text("Catch Phrase:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.company.catchPhrase)
-                        }
-
-                        HStack {
-                            Text("Business Strategy:")
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text(user.company.bs)
-                        }
-                    }
-
+                    companyInformationField(user)
                     // Fetch Button
-                    Section {
-                        Button(action: {
-                            let userID = Int.random(in: 1 ... 10)
-                            userViewModel.fetchUsers(id: userID)
-                        }) {
-                            HStack {
-                                Spacer()
-                                Text("Fetch Another User")
-                                    .foregroundColor(.blue)
-                                    .fontWeight(.bold)
-                                Spacer()
-                            }
-                        }
-                    }
+                    fetchButton()
                 } else {
-                    Section {
-                        HStack {
-                            Spacer()
-                            ProgressView("Loading User...")
-                            Spacer()
-                        }
-                    }
+                    loadingField()
                 }
             }
             .navigationTitle("User Detail")
@@ -139,6 +33,135 @@ struct UserDetailView: View {
             }
         }
     }
+
+    @ViewBuilder
+    private func userInformationField(_ user: User) -> some View {
+        Section(header: Text("User Information")) {
+            HStack {
+                Text("ID:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text("\(user.id)")
+            }
+
+            HStack {
+                Text("Name:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.name)
+            }
+
+            HStack {
+                Text("Username:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.userName)
+            }
+
+            HStack {
+                Text("Email:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.email)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func addressField(_ user: User) -> some View {
+        Section(header: Text("Address")) {
+            HStack {
+                Text("Street:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.address.street)
+            }
+
+            HStack {
+                Text("Suite:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.address.suite)
+            }
+
+            HStack {
+                Text("City:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.address.city)
+            }
+
+            HStack {
+                Text("Zipcode:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.address.zipcode)
+            }
+
+            HStack {
+                Text("Geo Coordinates:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text("Lat \(user.address.geo.latitude), Lng \(user.address.geo.longitude)")
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func companyInformationField(_ user: User) -> some View {
+        Section(header: Text("Company Information")) {
+            HStack {
+                Text("Company Name:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.company.name)
+            }
+
+            HStack {
+                Text("Catch Phrase:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.company.catchPhrase)
+            }
+
+            HStack {
+                Text("Business Strategy:")
+                    .fontWeight(.bold)
+                Spacer()
+                Text(user.company.bs)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func fetchButton() -> some View {
+        Section {
+            Button(action: {
+                let userID = Int.random(in: 1 ... 10)
+                userViewModel.fetchUsers(id: userID)
+            }) {
+                HStack {
+                    Spacer()
+                    Text("Fetch Another User")
+                        .foregroundColor(.blue)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func loadingField() -> some View {
+        Section {
+            HStack {
+                Spacer()
+                ProgressView("Loading User...")
+                Spacer()
+            }
+        }
+    }
+
 }
 
 #Preview {
